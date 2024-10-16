8a4faad9c0df04b60f8db846469876505bc6e2bd
root <root@Gadjishka-PC.localdomain>
2024-10-16 15:49:17 +0300

    Input fix

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 int main(){
  int a = 0;
  std::cin >> a;
+ if (std::cin.good()){
+  std::cerr << "Good!\n";
+ }
+ if (std::cin.eof()){
+  std::cerr << "EOF!\n";
+ }
+ if (std::cin.fail()){
+  std::cerr << "FAIL!\n";
+ }
+ if (std::cin.bad()){
+  std::cerr << "Bad!\n";
+ }
  std:: cout << a << "\n";
 }
