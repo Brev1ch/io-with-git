e5090d8cf3329a769e102d4f29f504c121b8aefe
root <root@Gadjishka-PC.localdomain>
2024-10-16 15:54:57 +0300

    Git reading again

--- main.cpp
+++ main.cpp
@@ -8,11 +8,14 @@ int main(){
  if (std::cin.eof()){
   std::cerr << "EOF!\n";
  }
+ if (std::cin.bad()){
+  std::cerr << "BAD!\n";
+ }
  if (std::cin.fail()){
   std::cerr << "FAIL!\n";
- }
- if (std::cin.bad()){
-  std::cerr << "Bad!\n";
+  std::cin.clear();
+  std::cin.ignore(1);
+  std::cin >> a;
  }
  std:: cout << a << "\n";
 }
